40acb636776dbd948e5d412ae63b772d1c8459d1
AzzraelCode <azzrael@bk.ru>
2024-02-18 19:56:03 +0300

    В ответ на комментарий https://www.youtube.com/watch?v=8SY-G0Hk64Y&lc=Ugxh1doXi5r-k4fEhK14AaABAg

--- videos/V5WebsocketPublic.py
+++ videos/V5WebsocketPublic.py
@@ -1,4 +1,6 @@
 from time import sleep
+
+import requests
 from pybit.unified_trading import WebSocket
 
 def handle_message(m):
@@ -8,11 +10,32 @@ def handle_ticker(m):
     d = m.get('data', {})
     print(d['symbol'], d['lastPrice'], sep=":")
 
+def subscribe_all_inst(ws : WebSocket):
+    """
+    Подписка на SPOT на все торгуемые к USDT пары,
+    с разбивкой на 10 аргументов
+    в ответ на комментарий
+    https://www.youtube.com/watch?v=8SY-G0Hk64Y&lc=Ugxh1doXi5r-k4fEhK14AaABAg
+    :param ws:
+    :return:
+    """
+    url = "https://api.bybit.com/v5/market/instruments-info"
+    l = requests.get(url, dict(category='spot')).json().get('result', {}).get('list', [])
+    symbols = [ s['symbol'] for s in l if s['quoteCoin'] == 'USDT' and s['status'] == 'Trading']
+
+    args_limit = 10
+    for i in range(0, len(symbols), args_limit):
+        ws.ticker_stream(symbol=(symbols[i:i + args_limit]), callback=handle_ticker)
+        sleep(0.5)
+
 def main():
+
     ws = WebSocket(
         testnet=False,
         channel_type="spot",
     )
+
+    # subscribe_all_inst(ws)
 
     ws.ticker_stream(symbol=[
         "BTCUSDT",
